366751381d38e1e8773392c9cc9286c738df780a
NguyenHaiNam1e1 <hainamnguyen1e1@gmail.com>
2020-12-01 22:39:28 +0700

    admin

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\admin\CategoryController;
+use App\Http\Controllers\Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,27 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/', [HomeController::class,'index']);
+
+
+Route::get('/detail', [HomeController::class,'detail']);
+
+
+
+
+//admin
+Route::get('/admin', [HomeController::class,'admin']);
+
+//category
+Route::get('/admin/category', [CategoryController::class,'index']);
+Route::get('/admin/delete_category/{id}', [CategoryController::class,'deleteCategory']);
+Route::get('/admin/show_category/{id}', [CategoryController::class,'showCategory']);
+Route::get('/admin/add_category', [CategoryController::class,'addCategory']);
+
+Route::post('/admin/save_category', [CategoryController::class,'saveCategory']);
+Route::post('/admin/update_category/{id}', [CategoryController::class,'updateCategory']);
